6fe2f6e3727fd6799635db3fcb3a77420824cbb8
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-23 01:06:08 -0500

    prompts user for data and col names

--- project2/project2.py
+++ project2/project2.py
@@ -1,7 +1,14 @@
 # OBJECTIVE: Display statistics, table, and histogram of a set of cities and population of each city.
-# TODO: prompt user for title of the data and output to the user
-# TODO: prompt user for headers of the two columns of the table
+# prompt user for title of the data and output to the user
+title = input("Enter a title for the data: ")
+print(f'You entered: {title}')
+# prompt user for headers of the two columns of the table
+colOneHeader = input("Enter the column 1 header:")
+print(f'You entered: {colOneHeader}')
+colTwoHeader = input("Enter the column 2 header:")
+print(f'You entered: {colTwoHeader}')
 # TODO: prompt user for data points, string, int for city and population
+
 # TODO: output min, max, and mean of the population data
 # TODO: output data in a formatted table
 # TODO: output data in a histogram; print one user-specified char for 100k peeps
