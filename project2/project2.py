2e9c68b55ba043b48aa123d994eeae4f7f886486
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-23 00:56:44 -0500

    adds sudo code

---
+++ project2/project2.py
@@ -0,0 +1,7 @@
+# OBJECTIVE: Display statistics, table, and histogram of a set of cities and population of each city.
+# TODO: prompt user for title of the data and output to the user
+# TODO: prompt user for headers of the two columns of the table
+# TODO: prompt user for data points, string, int for city and population
+# TODO: output min, max, and mean of the population data
+# TODO: output data in a formatted table
+# TODO: output data in a histogram; print one user-specified char for 100k peeps
