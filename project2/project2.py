1c3c5ae6b69198879c2b16b0c0502af94fa36160
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-25 21:25:35 -0500

    adds city table output
    iterates over the city data into a table
    
    TODO: formatting

--- project2/project2.py
+++ project2/project2.py
@@ -1,13 +1,16 @@
 # OBJECTIVE: Display statistics, table, and histogram of a set of cities and population of each city.
+
 # prompt user for title of the data and output to the user
 from __future__ import division
 title = input("Enter a title for the data: ")
 print(f'You entered: {title}')
+
 # prompt user for headers of the two columns of the table
 colOneHeader = input("Enter the column 1 header:")
 print(f'You entered: {colOneHeader}')
 colTwoHeader = input("Enter the column 2 header:")
 print(f'You entered: {colTwoHeader}')
+
 # TODO: prompt user for data points, string, int for city and population
 cityData = []
 cityPop = []
@@ -20,7 +23,7 @@ while(isDone != True):
         name, pop = dataInput.split(',')
         print(f'City: {name}')
         print(f'Population: {pop}')
-        dataPoint = {str(name), int(pop)}
+        dataPoint = {str(name), str(pop)}
         cityData.append(dataPoint)
         cityPop.append(int(pop))
 
@@ -32,15 +35,27 @@ def cityStatistics(cityPop):
     maxValue = max(cityPop)
     minValue = min(cityPop)
     avgValue = sum(cityPop)/len(cityPop)
+    print('')
     print('Population Statistics')
     print(f'Minimum: {minValue}')
     print(f'Maximum: {maxValue}')
     print(f'Mean: {avgValue:.3f}')
+    print('')
 
 
+def cityTable(cityData, title, colOneHeader, colTwoHeader):
+    print(title)
+    print(f'{colOneHeader}   |   {colTwoHeader}')
+    print('-----------------------------------------')
+    for pop, city in cityData:
+        print(f'{city}    |    {pop}')
+
+# uncommment to debug
 # print(cityPop)
 # print(cityData)
-cityStatistics(cityPop)
 
+
+cityStatistics(cityPop)
 # TODO: output data in a formatted table
+cityTable(cityData, title, colOneHeader, colTwoHeader)
 # TODO: output data in a histogram; print one user-specified char for 100k peeps
